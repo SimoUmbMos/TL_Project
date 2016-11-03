25e62acfcd622df7cfeed05ad669ba8f7d98e832
SimoUmbMos <sorariku95@hotmail.gr>
2016-11-03 04:34:52 +0200

    added 2 function: map mark for quest,distance of 2 location

--- app/src/main/java/com/tlproject/omada1/tl_project/MapsActivity.java
+++ app/src/main/java/com/tlproject/omada1/tl_project/MapsActivity.java
@@ -3,6 +3,7 @@ package com.tlproject.omada1.tl_project;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -15,7 +16,10 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.tlproject.omada1.tl_project.GPSTrack.GPSTracker;
 import com.tlproject.omada1.tl_project.Model.User;
 
@@ -75,9 +79,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             LatLng Loc = new LatLng(Lat,Long);
             mMap.moveCamera(CameraUpdateFactory.newLatLng(Loc));
             mMap.animateCamera(zoom);
-        /* Add a marker in Sydney
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        */
     }
 
     @Override
@@ -112,5 +113,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Intent intent=new Intent(MapsActivity.this,ProfileActivity.class);
         intent.putExtra("User", Curruser.ToString());
         startActivity(intent);
+    }
+    void setquestonmap(double lat,double lng){
+        mMap.clear();
+        mMap.addCircle(new CircleOptions().center(new LatLng(lat,lng)).radius(15).strokeColor(Color.RED).fillColor(Color.BLUE));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(lat,lng)).title("Current Quest"));
+    }
+    float distof(double lat1,double lng1,double lat2,double lng2){
+        Location loc1,loc2;
+        loc1=new Location("");
+        loc1.setLatitude(lat1);
+        loc1.setLongitude(lng1);
+        loc2=new Location("");
+        loc2.setLatitude(lat2);
+        loc2.setLongitude(lng2);
+        return loc1.distanceTo(loc2);
     }
 }
