577ba430569b4ea41fe0f3f262145d65979b1908
SimoUmbMos <sorariku95@hotmail.gr>
2016-11-04 16:40:00 +0200

    user model have desc

--- app/src/main/java/com/tlproject/omada1/tl_project/Model/User.java
+++ app/src/main/java/com/tlproject/omada1/tl_project/Model/User.java
@@ -1,19 +1,48 @@
 package com.tlproject.omada1.tl_project.Model;
 
 public class User {
-    private String password,username;
-    private int lvl,exp;
+    /**
+     * String with the users password
+     */
+    private String password;
+    /**
+     * String with the users username
+     */
+    private String username;
+    /**
+     * Int with the users Level
+     */
+    private int lvl;
+    /**
+     * Int with the users experience point
+     */
+    private int exp;
+    /**
+     *
+     */
     public User(){
         username="test123";
         password="test123";
         lvl=2;
         exp=1000;
     }
+    /**
+     * Return the users username
+     */
     public String getUsername(){
         return username;
     }
+    /**
+     * Return the users Level
+     */
     public int getLvl(){return lvl;}
+    /**
+     * Return the users experience point
+     */
     public int getExp(){return exp;}
+    /**
+     * Set user data from a string with a format like User.toString();
+     */
     public void setUser(String userS){
         char c;
         String tempS="";
@@ -45,12 +74,21 @@ public class User {
         }
         exp=Integer.parseInt(tempS);
     }
+    /**
+     * Compare the given username with the user username
+     */
     public boolean exist(String Username) {
         return username.equals(Username);
     }
+    /**
+     *  Compare the given password with the user password
+     */
     public boolean PasswordEq(String Password) {
         return password.equals(Password);
     }
+    /**
+     * Return User data as String
+     */
     public String ToString(){
         return (username+";"+password+";"+String.valueOf(lvl)+";"+String.valueOf(exp)+";");
     }
